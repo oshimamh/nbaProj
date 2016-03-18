d989b7df25f6c90147c24cbd2897f3295047308e
Mitch Oshima <oshimamh@g.cofc.edu>
2016-03-18 11:07:20 -0400

    Removed some code used to work through making the shotChart functions

--- shotCharts.R
+++ shotCharts.R
@@ -4,16 +4,19 @@ library(coefplot)
 library(ggmap)
 library(png)
 library(rjson)
+library(grid)
+library(gridExtra)
 
-basicShotChart <- function(playerShotData){
-  shotChart <- ggplot(playerShotData,
-                      aes(x=LOC_X, y=LOC_Y)
-  ) +
-    geom_point(
-      aes(colour = EVENT_TYPE)
-    )
+basicShotChart <- function(playerName){
+  df <- getShotData(playerName)
+  shotChart <- ggplot(df,aes(x=LOC_X, y=LOC_Y)) +
+    geom_point(aes(colour = EVENT_TYPE))+
+    xlim(-250,250)+
+    ylim(-50,420)
   return(shotChart)
 }
+
+basicShotChart("Klay Thompson")
 
 invlogit <- function(x){
   1/(1+exp(-x))
@@ -26,14 +29,6 @@ hardenData = getShotData("James Harden")
 hardenMod1 = glm(EVENT_TYPE~LOC_X+LOC_Y-1, data = hardenData, family = binomial)
 summary(hardenMod1)
 coefplot(hardenMod1, trans = invlogit, title = "Probability of a Made Shot")
-XYCoord = hardenData[,c("LOC_X","LOC_Y")]
-ggplot(data = heatMap, aes(x=LOC_X, y=LOC_Y, color=prediction)) + 
-  geom_point() + 
-  scale_color_gradient(low="blue",high="red")
-ggplot(data = heatMap, aes(x=LOC_X, y=LOC_Y, z=prediction)) + 
-  geom_point(aes(color = prediction)) +
-  geom_density2d()+
-  scale_color_gradient(low="blue",high="red")
 
 shotModelFit <- function(player){
   df <- getShotData(player)
@@ -47,21 +42,13 @@ densityShotChart <- function(player){
   g <- rasterGrob(img, interpolate=TRUE)
   df <- shotModelFit(player)
   shotChart <- ggplot(data = df, aes(x=LOC_X, y=LOC_Y, z=PROB)) + 
-#      annotation_custom(g, -250, 250, -50, 420)+
-      annotation_custom(g)+
+      annotation_custom(g, -275, 275, -35, 420)+
       xlim(-250,250)+
       ylim(-50,420)+
-      stat_density2d(geom = "polygon", n=200, aes(fill=..level.., alpha = 1/2))+
-      geom_point(aes(color = PROB, alpha = 1/2)) +
-      scale_color_gradient(low="yellow",high="red")
+      geom_point(aes(color = PROB)) +
+      scale_color_gradient(low="yellow",high="red")+
+      stat_density2d(geom = "polygon", n=200, aes(fill=..level.., alpha = 1/2))
   return(shotChart)
 }
 
-#ggplot(shotDataf, aes(x=LOC_X, y=LOC_Y)) + 
-densityShotChart("Stephen Curry")
-
-courtImgURL <- "https://thedatagame.files.wordpress.com/2016/03/nba_court.jpg"
-grid.raster(readJPEG(getURLContent(courtImgURL)))
-
-img <- readPNG("nba_court.png")
-g <- rasterGrob(img, interpolate=TRUE)
+densityShotChart("Klay Thompson")
